ae0c78b009dffba3b7306e4d1e68502260e8eed7
DiegoMD4 <diegolink4@hotmail.com>
2026-05-24 09:21:07 -0600

    change: make connection to mysql asynchronous

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/mysql2"
 import mysql from "mysql2/promise"
 import * as schema from "./schema/schema"
 
-const connection =  mysql.createPool({
+const connection =  await mysql.createConnection({
   uri: process.env.DATABASE_URL,
 })
 
